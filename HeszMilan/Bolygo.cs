d30a1fd8ec97b0af0f6d7db212b1e39beb19c587
ny19heszm <ny19heszm@203-07>
2023-10-24 12:24:38 +0200

    Add project files.

---
+++ HeszMilan/Bolygo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeszMilan
+{
+    internal class Bolygo
+    {
+        public string Nev { get; set; }
+        public int HoldSzama { get; set; }
+        public string TerfogatAranyF { get; set; }
+
+
+        public Bolygo(string sor)
+        {
+
+            var v = sor.Split(';');
+            Nev = v[0];
+            HoldSzama = int.Parse(v[1]);
+            TerfogatAranyF =v[2];
+
+        }
+       
+    }
+}
---
+++ HeszMilan/Program.cs
@@ -0,0 +1,65 @@
+﻿using HeszMilan;
+
+namespace HeszMilan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Bolygo> bolygo = new();
+            using StreamReader sr = new(path: @"..\..\..\src\solsys.txt",
+            encoding: System.Text.Encoding.UTF8);
+            {
+                while (!sr.EndOfStream) bolygo.Add(new Bolygo(sr.ReadLine()));
+  
+            }
+            
+
+
+            Console.WriteLine("1. Feladat:");
+            Console.WriteLine($"{bolygo.Count} bolygó van a naprendszerben");
+
+            Console.WriteLine("2. Feladat:");
+            var atlag = bolygo.Average(p => p.HoldSzama);
+            Console.WriteLine($"a naprendszerben egy bolygónak átlagosan {atlag} holdja van");
+
+            Console.WriteLine("3. Feladat:");
+            var legnagyobbTerfogatu = bolygo.OrderByDescending(p => p.TerfogatAranyF)
+                .First();
+            Console.WriteLine($"a legnagyobb térfogatú bolygó a {legnagyobbTerfogatu.Nev}");
+
+            Console.WriteLine("4. Feladat:");
+            Console.WriteLine("Írd be a kerestt bolygó nevét: ");
+            string keres = Console.ReadLine();
+            foreach (Bolygo item in bolygo)
+            {
+                if (keres == item.Nev)
+                {
+                    Console.WriteLine($"Keresett bolygó {keres} benne van.");
+                }
+                else
+                {
+                    Console.WriteLine($"sajnos nincs ilyen nevű bolygó a naprendszerben");
+                }
+            }
+
+            Console.WriteLine("5. Feladat:");
+            Console.WriteLine("Írj be egy egész számot: ");
+            int szam = 0;
+            var keresH = bolygo.Where(p => p.HoldSzama > szam);
+            if (keresH.Count() != 0)
+            {
+                Console.WriteLine($"a következő bolygónak van {keresH}-nál/nél több holdja: {i.Nev}");
+                foreach (var f in keresH)
+                {
+                    Console.Write($"'{f.}';");
+                }
+            }
+            else Console.WriteLine($"Nincs ilyen ο(=•ω＜=)ρ⌒☆");
+        }
+    }
+
+
+
+}
+    
